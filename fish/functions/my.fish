c2a5f87f2a02a55d6b2d84fd8a9873322a4aba33
Mohammad AbuShady <mohammad@abushady.com>
2020-09-03 15:15:04 +0200

    Move my fish functions to my.fish

---
+++ fish/functions/my.fish
@@ -0,0 +1,39 @@
+
+abbr ghost 'nvim +GhostStart'
+
+
+function fish_greeting
+  neofetch
+end
+
+function set_window_title
+  echo -ne "\e]0;$argv\007"
+end
+
+bind \cc 'commandline ""'
+
+
+function read_text
+  set -l text (echo $argv | tr ' ' '+')
+  curl -s "https://text-to-speech-demo.ng.bluemix.net/api/v3/synthesize?text=$text&voice=en-US_EmilyV3Voice&download=true&accept=audio%2Fmp3" -o /tmp/tts; and cvlc --play-and-exit --no-repeat /tmp/tts > /dev/null; and rm /tmp/tts
+end
+
+
+function fish_greeting
+  neofetch
+end
+
+#Abbreviations
+
+abbr checkout 'git checkout'
+abbr commit 'git commit'
+abbr add 'git add'
+abbr rebase 'git rebase origin/master -i'
+abbr fetch 'git fetch -p'
+abbr ff 'git merge --ff-only'
+abbr pf 'git push origin HEAD --force-with-lease'
+abbr fixup 'git commit --fixup'
+abbr squash 'git rebase origin/master -i --autosquash'
+abbr amn 'git commit --amend --no-edit'
+
+alias oneline "xsel -b -o | tr -d '\n' | tr -s ' ' | xsel -b -i"
